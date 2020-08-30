0fdb83ccb874b73be449f6584b68e9970f6895fa
yves848 <yves.godart@gmail.com>
2020-08-30 22:27:09 +0200

    get counter

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const page_url = 'https://www.2ememain.be/l/informatique-logiciels/portables-et-notebooks/p/1/#Language:fr-BE|postcode:6061';
+const page_url = 'https://www.2ememain.be/l/informatique-logiciels/portables-et-notebooks/p/1/#Language:fr-BE|postcode:6061|limit:100';
 
 async function getNotebooks() {
     const {data} = await axios.get(page_url);
@@ -10,8 +10,9 @@ async function getNotebooks() {
     //console.log(pagination);
     //#content > div.mp-Page-element.mp-Page-element--main
     const spans = $('.mp-Filter-counter');
-    const $spans = $(spans);
-    console.log($spans);
+    const $spans = $($(spans).contents()[2]);
+    
+    console.log($spans.text());
     const table = $('ul.mp-Listings.mp-Listings--list-view');
     //const table = $('.mp-Listing--list-item');
     table.find('.mp-Listing--list-item').each((i, element) => {
